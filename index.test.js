cf956cec5e1008f6094337afc140525dafe7f9af
Campbell Conyers <campbell.conyers@appscore.net>
2022-05-03 22:11:38 +0100

    added framework for index.js and index.test.js

---
+++ index.test.js
@@ -0,0 +1,12 @@
+import sinon from 'sinon';
+import chai from 'chai';
+import sinonChai from 'sinon-chai';
+chai.use(sinonChai);
+const expect = chai.expect;
+
+// import {  } from './index.js';
+
+describe('Tests', () => {
+  it('', () => {
+  });
+});
